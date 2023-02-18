5b4733d251dcbf27e42e1fc18a4cb81cc75d21e1
sm2k4 <shrinibasmahanta2004@gmail.com>
2023-02-18 17:59:15 +0530

    completes getComputerChoice,playRound function.

---
+++ src/script.js
@@ -0,0 +1,35 @@
+
+// This function returns a random rock/paper/scissors choice from the choices array.
+// For randomisation uses random function.
+function getComputerChoice(){
+    
+    const choices = ["rock","scissors","paper"];
+    let resInd = Math.floor(Math.random()*3);  
+    
+    return choices[resInd];
+};
+//Plays a round of the game..
+function playRound(){
+    let playerChoice = prompt("Enter your choice: ");
+    playerChoice = playerChoice.toLowerCase();
+    console.log("Player chooses: "+playerChoice);
+
+    const compChoice = getComputerChoice();
+    console.log("Computer chooses: "+compChoice);
+
+    //These store the wincases for the player {playerChoice:compChoice}.
+    const playerWinCase = {"rock":"scissors","scissors":"paper","paper":"rock"};
+    
+    //decides who wins the game...
+    if (playerChoice === compChoice){
+        return "This is a tie! No one wins...";
+    };
+    if (playerWinCase[playerChoice] === compChoice){
+        return "Yay...You have defeated the computer.....congrats on your win...";
+    }else{
+        return "Oops....You have been defeated......The computer wins...";
+    };
+};
+
+console.log(playRound())
+
